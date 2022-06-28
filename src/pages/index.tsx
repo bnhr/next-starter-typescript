6128f6593f7a7b4350b9ba038174d7200aa7fb1a
Heru Gunawan <heru07g@gmail.com>
2022-06-28 15:16:48 +0700

    chore: migrate to pnpm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '@/components/Layout/Layout'
 
 function Home() {
-  return <Layout>hello from next.js</Layout>
+  return <Layout>hello from Next.js</Layout>
 }
 
 export default Home
